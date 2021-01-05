1c7dc4a1b1cf5383d4366c88ac17b7a616bfb2c2
Jezonfdo <jezonfernando.creator@gmail.com>
2021-01-05 22:13:16 +0530

    Data fetch

--- src/App.js
+++ src/App.js
@@ -30,10 +30,10 @@ class App extends Component {
       return hero.name.toLowerCase().includes(searchfield.toLowerCase()) || hero.heroName.toLowerCase().includes(searchfield.toLowerCase());
     })
     return !heros.length ?
-      <h1>Loading</h1> :
+      <h1 className='tc'>Loading</h1> :
       (
         <div className='tc'>
-          <h1 className='f1'>RoboFriends</h1>
+          <h1>Marvel Cinematic Universe</h1>
           <SearchBox searchChange={this.onSearchChange}/>
           <CardsList heros={filteredheros} />
         </div>
